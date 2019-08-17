e151d3c7de8d2bbf3d777638b5ea448b7a6fd858
vladimirsan <vladimirsan@gmail.com>
2019-08-16 17:09:35 -0700

    linked lists

---
+++ curriculum/class-05/linked-list/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const LinkedList = require('./linked-list');
+
+// list !== linked list
+
+const linkedList = new LinkedList();
+
+linkedList.insertAtHead(10);
+linkedList.insertAtHead(20);
+linkedList.insertAtHead(30);
+
+
+linkedList.traverse();
+
+
+
+
